e0c46ca33cf653621ad429b92ed832af3bfe4350
Laura-Neff <ljneff@gmail.com>
2021-04-25 16:00:18 -0400

    Finished part 5.

--- reinforcement/qlearningAgents.py
+++ reinforcement/qlearningAgents.py
@@ -128,18 +128,11 @@ class QLearningAgent(ReinforcementAgent):
         legalActions = self.getLegalActions(state)
         action = None
         "*** YOUR CODE HERE ***"
-        # self.epsilon
-        # p = util.flipCoin() #Flip a coin
         if(util.flipCoin(self.epsilon)): #If probability is determined to be epsilon
-            random.choice(legalActions)
-        else:
-            print ("Act on current policy")
+            action = random.choice(legalActions) #Choose a random action
+        else: #If probability is anything other than epsilon
+            action = self.computeActionFromQValues(state) #Return best policy action
 
-
-
-
-
-        #util.raiseNotDefined()
 
         return action
 
