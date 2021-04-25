548da8fabce780d3cb3437d19837785e9fe45a30
Laura-Neff <ljneff@gmail.com>
2021-04-25 16:16:50 -0400

    Finished q6.

--- reinforcement/analysis.py
+++ reinforcement/analysis.py
@@ -66,9 +66,10 @@ def question3e():
     # If not possible, return 'NOT POSSIBLE'
 
 def question6():
-    answerEpsilon = None
-    answerLearningRate = None
-    return answerEpsilon, answerLearningRate
+    answerEpsilon = 0
+    answerLearningRate = 0
+    #return answerEpsilon, answerLearningRate
+    return "NOT POSSIBLE"
     # If not possible, return 'NOT POSSIBLE'
 
 if __name__ == '__main__':
